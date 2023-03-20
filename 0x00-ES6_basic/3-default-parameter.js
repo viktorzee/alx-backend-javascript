63d786c3e7428e9d224c120be5fb002084e1df7e
Victor Opara <monsieurviktor@yahoo.com>
2023-03-20 13:45:38 +0000

    first batch

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,6 @@
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+  expansion1989 === undefined && expansion1989 = 89
+
+  expansion2019 === undefined && expansion2019 = 19
+  return initialNumber + expansion1989 + expansion2019;
+}
